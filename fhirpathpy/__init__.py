88f3634bb7d2140e6c198b2f3bead54fb34d1d75
Sergey Ufocoder <xufocoder@gmail.com>
2020-02-26 19:35:39 +0300

    Add `__init__` to `fhirpathpy`

---
+++ fhirpathpy/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'fhirpathpy'
+__version__ = '0.0.1-alpha'
+__author__ = 'beda.software'
+__license__ = 'None'
+__copyright__ = 'Copyright 2020 beda.software'
+
+# Version synonym
+VERSION = __version__
