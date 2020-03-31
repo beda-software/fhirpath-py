9808bb6405362405af42f0d085e05ea60bef9197
Sergey Ufocoder <xufocoder@gmail.com>
2020-03-31 11:15:34 +0300

    Add `fhirpathpy.engine.evaluators` module

---
+++ fhirpathpy/engine/__init__.py
@@ -0,0 +1,174 @@
+import json
+import numbers
+import fhirpathpy.engine.util as util
+from fhirpathpy.engine.evaluators import evaluators
+from fhirpathpy.engine.invocations import invocations
+
+
+def checkIntegerParam(val):
+    data = util.valData(val)
+    if int(data) != data:
+        raise Exception("Expected integer, got: " + json.dumps(data))
+    return data
+
+
+def checkNumberParam(val):
+    data = util.valData(val)
+    if not isinstance(data, numbers.Number):
+        raise Exception("Expected number, got: " + json.dumps(data))
+    return data
+
+
+def checkBooleanParam(val):
+    data = util.valData(val)
+    if data == True or data == False:
+        return data
+    raise Exception("Expected boolean, got: " + json.dumps(data))
+
+
+def checkStringParam(val):
+    data = util.valData(val)
+    if not isinstance(data, str):
+        raise Exception("Expected string, got: " + json.dumps(data))
+    return data
+
+
+def doEval(ctx, parentData, node):
+    nodeType = node["type"]
+
+    if nodeType in evaluators:
+        evaluator = evaluators.get(nodeType)
+        return evaluator(ctx, parentData, node)
+
+    raise Exception("No " + nodeType + " evaluator ")
+
+
+def doInvoke(ctx, fnName, data, rawParams):
+    if isinstance(fnName, list) and len(fnName) == 1:
+        fnName = fnName[0]
+
+    if type(fnName) != str or not fnName in invocations:
+        raise Exception("Not implemented: " + str(fnName))
+
+    invocation = invocations[fnName]
+
+    if not "arity" in invocation:
+        if rawParams is None or util.isEmpty(rawParams):
+            res = invocation["fn"](util.arraify(data))
+            return util.arraify(res)
+
+        raise Exception(fnName + " expects no params")
+
+    paramsNumber = 0
+    if not util.isEmpty(rawParams):
+        paramsNumber = len(rawParams)
+
+    if not paramsNumber in invocation["arity"]:
+        print(fnName + " wrong arity: got " + str(paramsNumber))
+        return []
+
+    params = []
+    argTypes = invocation["arity"][paramsNumber]
+
+    for i in range(0, paramsNumber):
+        tp = argTypes[i]
+        pr = rawParams[i]
+        params.append(makeParam(ctx, data, tp, pr))
+
+    params.insert(0, data)
+
+    if "nullable" in invocation:
+        if any(util.isNullable(x) for x in params):
+            return []
+
+    res = invocation["fn"](*params)
+
+    return util.arraify(res)
+
+
+paramCheckTable = {
+    "Integer": checkIntegerParam,
+    "Number": checkNumberParam,
+    "Boolean": checkBooleanParam,
+    "String": checkStringParam,
+}
+
+
+def makeParam(ctx, parentData, nodeType, param):
+    ctx["currentData"] = parentData
+
+    if nodeType == "Expr":
+
+        def func(data):
+            return doEval(ctx, util.arraify(data), param)
+
+        return func
+
+    if nodeType == "AnyAtRoot":
+        return doEval(ctx, ctx.dataRoot, param)
+
+    if nodeType == "Identifier":
+        if param.type == "TermExpression":
+            return param.text
+
+        raise Exception("Expected identifier node, got " + json.dumps(param))
+
+    res = doEval(ctx, parentData, param)
+
+    if nodeType == "Any":
+        return res
+
+    if isinstance(nodeType, list):
+        if len(res):
+            return []
+        else:
+            nodeType = nodeType[0]
+
+    if len(res) > 1:
+        raise Exception(
+            "Unexpected collection"
+            + json.dumps(res)
+            + "; expected singleton of type "
+            + nodeType
+        )
+
+    if len(res) == 0:
+        return []
+
+    if nodeType not in paramCheckTable:
+        raise Exception("Implement me for " + nodeType)
+
+    check = paramCheckTable[nodeType]
+
+    return check(res[0])
+
+
+def infixInvoke(ctx, fnName, data, rawParams):
+    if not fnName in invocations or not "fn" in invocations[fnName]:
+        raise Exception("Not implemented " + fnName)
+
+    invocation = invocations[fnName]
+    paramsNumber = len(rawParams)
+
+    if paramsNumber != 2:
+        raise Exception("Infix invoke should have arity 2")
+
+    argTypes = invocation["arity"][paramsNumber]
+
+    if argTypes is not None:
+        params = []
+
+        for i in range(0, paramsNumber):
+            tp = argTypes[i]
+            pr = rawParams[i]
+            params.append(makeParam(ctx, data, tp, pr))
+
+        if "nullable" in invocation:
+            if any(util.isNullable(x) for x in params):
+                return []
+
+        res = invocation["fn"](params[0], params[1])
+        return util.arraify(res)
+
+    print(fnName + " wrong arity: got " + paramsNumber)
+    return []
---
+++ fhirpathpy/engine/evaluators/__init__.py
@@ -0,0 +1,275 @@
+from functools import reduce
+
+import re
+import json
+import fhirpathpy.engine as engine
+import fhirpathpy.engine.util as util
+import fhirpathpy.engine.nodes as nodes
+
+
+def BooleanLiteral(ctx, parentData, node):
+    if node["text"] == "true":
+        return [True]
+    return [False]
+
+
+def NumberLiteral(ctx, parentData, node):
+    return [float(node["text"])]
+
+
+def Identifier(ctx, parentData, node):
+    return [re.sub(r"(^\"|\"$)", "", node["text"])]
+
+
+def InvocationTerm(ctx, parentData, node):
+    return engine.doEval(ctx, parentData, node["children"][0])
+
+
+def InvocationExpression(ctx, parentData, node):
+    return list(
+        reduce(
+            lambda accumulator, children: engine.doEval(ctx, accumulator, children),
+            node["children"],
+            parentData,
+        )
+    )
+
+
+def ParamList(ctx, parentData, node):
+    # we do not eval param list because sometimes it should be passed as
+    # lambda/macro (for example in case of where(...)
+    return node
+
+
+def UnionExpression(ctx, parentData, node):
+    return engine.infixInvoke(ctx, "|", parentData, node["children"])
+
+
+def ThisInvocation(ctx, parentData, node):
+    return util.arraify(ctx["currentData"])
+
+
+def OpExpression(ctx, parentData, node):
+    op = node["terminalNodeText"][0]
+    return engine.infixInvoke(ctx, op, parentData, node["children"])
+
+
+def AliasOpExpression(mapFn):
+    def func(ctx, parentData, node):
+        op = node["terminalNodeText"][0]
+
+        if not op in mapFn:
+            raise Exception(
+                "Do not know how to alias " + op + " by " + json.dumps(mapFn)
+            )
+
+        alias = mapFn[op]
+        return engine.infixInvoke(ctx, alias, parentData, node["children"])
+
+    return func
+
+
+def TermExpression(ctx, parentData, node):
+    return engine.doEval(ctx, parentData, node["children"][0])
+
+
+def NullLiteral():
+    return []
+
+
+def ParenthesizedTerm(ctx, parentData, node):
+    return engine.doEval(ctx, parentData, node["children"][0])
+
+
+def LiteralTerm(ctx, parentData, node):
+    term = node["children"][0]
+
+    if term:
+        return engine.doEval(ctx, parentData, term)
+
+    return [node["text"]]
+
+
+def StringLiteral(ctx, parentData, node):
+    # Remove the beginning and ending quotes.
+    rtn = re.sub(r"^['\"]|['\"]$", "", node["text"])
+
+    rtn = rtn.replace("\\'", "'")
+    rtn = rtn.replace('\\"', '"')
+    rtn = rtn.replace("\\r", "\r")
+    rtn = rtn.replace("\\n", "\n")
+    rtn = rtn.replace("\\t", "\t")
+    rtn = rtn.replace("\\f", "\f")
+    rtn = rtn.replace("\\\\", "\\")
+
+    # TODO
+    #  rtn = rtn.replace(/\\(u\d{4}|.)/g, function(match, submatch) {
+    #     if (submatch.length > 1)
+    #       return String.fromCharCode('0x'+submatch.slice(1));
+    #     else
+    #       return submatch;
+
+    return [rtn]
+
+
+def QuantityLiteral(ctx, parentData, node):
+    valueNode = node["children"][0]
+    value = float(valueNode["terminalNodeText"][0])
+    unitNode = valueNode["children"][0]
+    unit = unitNode.terminalNodeText[0]
+    # Sometimes the unit is in a child node of the child
+    if unit is not None and len(unitNode["children"]) > 0:
+        unit = unitNode["children"][0]["terminalNodeText"][0]
+
+    return [nodes.FP_Quantity(value, unit)]
+
+
+def DateTimeLiteral(ctx, parentData, node):
+    dateStr = node["text"][:1]
+    return [nodes.FP_DateTime(dateStr)]
+
+
+def TimeLiteral(ctx, parentData, node):
+    timeStr = node["text"][:1]
+    return [nodes.FP_Time(timeStr)]
+
+
+def __memberReduce(model, key):
+    def func(acc, res):
+        res = nodes.ResourceNode.makeResNode(res)
+
+        childPath = ""  # res['path'] + '.' + key # TODO
+
+        if model:
+            defPath = model["pathsDefinedElsewhere"][childPath]
+            if defPath:
+                childPath = defPath
+
+        actualTypes = None
+        if model and model["choiceTypePaths"]:
+            actualTypes = model["choiceTypePaths"][childPath]
+
+        if isinstance(actualTypes, list):
+            # Use actualTypes to find the field's value
+            for actualType in actualTypes:
+                field = key + actualType
+                toAdd = res.data[field]
+                if toAdd:
+                    childPath = actualType
+                    break
+        else:
+            toAdd = res.data[key]
+
+        if util.isSome(toAdd):
+            if isinstance(toAdd, list):
+                mapped = list(
+                    map(lambda x: nodes.ResourceNode.makeResNode(x, childPath), toAdd)
+                )
+                acc = acc + mapped
+            else:
+                acc.append(nodes.ResourceNode.makeResNode(toAdd, childPath))
+            return acc
+        return acc
+
+    return func
+
+
+def MemberInvocation(ctx, parentData, node):
+    key = engine.doEval(ctx, parentData, node["children"][0])[0]
+    model = ctx["model"]
+
+    if isinstance(parentData, list):
+
+        if util.isCapitalized(key):
+            filtered = list(filter(lambda x: x["resourceType"] == key, parentData))
+            mapped = list(
+                map(lambda x: nodes.ResourceNode.makeResNode(x, key), filtered)
+            )
+            return mapped
+
+        return list(reduce(__memberReduce(model, key), parentData, []))
+
+    return []
+
+
+def IndexerExpression(ctx, parentData, node):
+    coll_node = node["children"][0]
+    idx_node = node["children"][1]
+
+    coll = engine.doEval(ctx, parentData, coll_node)
+    idx = engine.doEval(ctx, parentData, idx_node)
+
+    if util.isEmpty(idx):
+        return []
+
+    idxNum = int(idx[0])
+
+    if coll is not None and util.isSome(idxNum) and len(coll) > idxNum and idxNum >= 0:
+        return [coll[idxNum]]
+
+    return []
+
+
+def Functn(ctx, parentData, node):
+    return list(map(lambda x: engine.doEval(ctx, parentData, x), node["children"]))
+
+
+def FunctionInvocation(ctx, parentData, node):
+    args = engine.doEval(ctx, parentData, node["children"][0])
+    fnName = args[0]
+    args = args[1:]
+
+    rawParams = None
+    if isinstance(args, list) and len(args) > 0 and "children" in args[0]:
+        rawParams = args[0]["children"]
+
+    return engine.doInvoke(ctx, fnName, parentData, rawParams)
+
+
+def PolarityExpression(ctx, parentData, node):
+    sign = node["terminalNodeText"][0]
+    rtn = engine.doEval(ctx, parentData, node["children"][0])
+
+    if len(rtn) != 1:  # not yet in spec, but per Bryn Rhodes
+        raise Exception(
+            "Unary " + sign + " can only be applied to an individual number."
+        )
+
+    if not util.isNumber(rtn[0]):
+        raise Exception("Unary " + sign + " can only be applied to a number.")
+
+    if sign == "-":
+        rtn[0] = -rtn[0]
+
+    return rtn
+
+
+evaluators = {
+    "MemberInvocation": MemberInvocation,
+    "Identifier": Identifier,
+    "BooleanLiteral": BooleanLiteral,
+    "NumberLiteral": NumberLiteral,
+    "ParamList": ParamList,
+    "StringLiteral": StringLiteral,
+    "ThisInvocation": ThisInvocation,
+    "NullLiteral": NullLiteral,
+    "Functn": Functn,
+    "FunctionInvocation": FunctionInvocation,
+    "PolarityExpression": PolarityExpression,
+    "IndexerExpression": IndexerExpression,
+    "MembershipExpression": AliasOpExpression({"contains": "containsOp", "in": "inOp"}),
+    "ParenthesizedTerm": ParenthesizedTerm,
+    "InvocationTerm": InvocationTerm,
+    "LiteralTerm": LiteralTerm,
+    "TermExpression": TermExpression,
+    "UnionExpression": UnionExpression,
+    "InvocationExpression": InvocationExpression,
+    "InequalityExpression": OpExpression,
+    "AdditiveExpression": OpExpression,
+    "MultiplicativeExpression": OpExpression,
+    "EqualityExpression": OpExpression,
+    "OrExpression": OpExpression,
+    "ImpliesExpression": OpExpression,
+    "AndExpression": OpExpression,
+    "XorExpression": OpExpression,
+}
