9616bcc9af5b45c7f5a63c054c3ffe3b937d8414
Sergey Ufocoder <xufocoder@gmail.com>
2020-03-31 11:19:50 +0300

    Add `logic` invocations

---
+++ fhirpathpy/engine/invocations/logic.py
@@ -0,0 +1,60 @@
+def orOp(a, b):
+    if isinstance(b, list):
+        if a == True:
+            return True
+        if a == False:
+            return []
+        if isinstance(a, list):
+            return []
+    if isinstance(a, list):
+        if b == True:
+            return True
+        return []
+
+    return a or b
+
+
+def andOp(a, b):
+    if isinstance(b, list):
+        if a == True:
+            return []
+        if a == False:
+            return False
+        if isinstance(a, list):
+            return []
+
+    if isinstance(a, list):
+        if b == True:
+            return []
+        return False
+
+    return a and b
+
+
+def xorOp(a, b):
+    # If a or b are arrays, they must be the empty set.
+    # In that case, the result is always the empty set.
+    if isinstance(a, list) or isinstance(b, list):
+        return []
+
+    return (a and not b) or (not a and b)
+
+
+def impliesOp(a, b):
+    if isinstance(b, list):
+        if a == True:
+            return []
+        if a == False:
+            return True
+        if isinstance(a, list):
+            return []
+
+    if isinstance(a, list):
+        if b == True:
+            return True
+        return []
+
+    if a == False:
+        return True
+
+    return a and b
