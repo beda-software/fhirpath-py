065485b49e45e4ff2147ee8ee6b8196841757801
Sergey Ufocoder <xufocoder@gmail.com>
2020-04-15 11:36:59 +0300

    Add extra check for `navigation` invocation

--- fhirpathpy/engine/invocations/navigation.py
+++ fhirpathpy/engine/invocations/navigation.py
@@ -11,17 +11,26 @@ def create_reduce_children(ctx):
     def func(acc, res):
         data = util.get_data(res)
         res = create_node(res)
+
         if isinstance(data, list):
             data = dict((i, data[i]) for i in range(0, len(data)))
+
         if isinstance(data, dict):
             for prop in data.keys():
                 value = data[prop]
                 childPath = ""
+
                 if res.path is not None:
                     childPath = res.path + "." + prop
-                if model is not None and "pathsDefinedElsewhere" in model:
-                    if value in model["pathsDefinedElsewhere"]:
-                        childPath = model["pathsDefinedElsewhere"][childPath]
+
+                if (
+                    isinstance(model, dict)
+                    and "pathsDefinedElsewhere" in model
+                    and isinstance(value, (int, str))
+                    and value in model["pathsDefinedElsewhere"]
+                    and childPath in model["pathsDefinedElsewhere"]
+                ):
+                    childPath = model["pathsDefinedElsewhere"][childPath]
 
                 if isinstance(value, list):
                     mapped = [create_node(n, childPath) for n in value]
