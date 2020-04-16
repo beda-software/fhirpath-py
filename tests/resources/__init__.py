072c522ba04096291921118f45ad41db2a528ac2
Sergey Ufocoder <xufocoder@gmail.com>
2020-04-16 23:21:31 +0300

    Fix codestyle

--- tests/resources/__init__.py
+++ tests/resources/__init__.py
@@ -1,9 +1,10 @@
 import os
-import json 
+import json
 
 model_path = os.path.dirname(__file__)
 
 resources = {}
+
 
 def save_to_resources(resources, resource_filename):
     filepath = os.path.join(model_path, resource_filename)
@@ -11,8 +12,8 @@ def save_to_resources(resources, resource_filename):
         resources[resource_filename] = json.loads(paths_file.read())
 
 
-save_to_resources(resources, 'observation-example.json')
-save_to_resources(resources, 'patient-example.json')
-save_to_resources(resources, 'quantity-example.json')
-save_to_resources(resources, 'questionnaire-example.json')
-save_to_resources(resources, 'valueset-example-expansion.json')
+save_to_resources(resources, "observation-example.json")
+save_to_resources(resources, "patient-example.json")
+save_to_resources(resources, "quantity-example.json")
+save_to_resources(resources, "questionnaire-example.json")
+save_to_resources(resources, "valueset-example-expansion.json")
