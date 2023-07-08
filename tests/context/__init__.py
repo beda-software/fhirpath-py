4d390b3b1744433327298038563a3150cb7f49bc
Ilya Beda <ir4y.ix@gmail.com>
2023-07-08 15:33:52 +0400

    Add data tests #24

--- tests/context/__init__.py
+++ tests/context/__init__.py
@@ -1,7 +1,18 @@
-import tests.context.r4 as r4
-import tests.context.stu3 as stu3
+import os
+from collections import defaultdict
+import json
 
-models = {
-    "r4": r4.model,
-    "stu3": stu3.model,
-}
+import pathlib
+current_dir = pathlib.Path(__file__).parent.resolve()
+
+models = defaultdict(dict)
+
+dirs = [f for f in os.listdir(current_dir)]
+
+for d in dirs:
+    pd = os.path.join(current_dir, d)
+    if os.path.isdir(pd):
+        print(d)
+        for f in os.listdir(pd):
+            with open(os.path.join(pd,f)) as fd:
+                models[d][f[:-5]] = json.loads(fd.read())
