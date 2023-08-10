79e302f54a433ee95446451cf8d264084eb14d5b
Aleksey <alexzlk@ya.ru>
2023-08-10 13:13:12 -0300

    Convert path to str if DSL type

--- fhirpathpy/__init__.py
+++ fhirpathpy/__init__.py
@@ -64,6 +64,8 @@ def evaluate(resource, path, context={}, model=None):
     int: Description of return value
 
     """
+    if type(path) is DSL:
+        path = str(path)
     node = parse(path)
     return apply_parsed_path(resource, node, context, model)
 
