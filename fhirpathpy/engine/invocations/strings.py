def4eb511e53c890c5c753c9cb701af0a340429e
atuonufure <atuonufure@gmail.com>
2023-08-15 07:40:13 +0200

    Implement encode and decode invocations

--- fhirpathpy/engine/invocations/strings.py
+++ fhirpathpy/engine/invocations/strings.py
@@ -1,3 +1,4 @@
+import base64
 import re
 import fhirpathpy.engine.util as util
 
@@ -61,6 +62,52 @@ def split(ctx, coll, delimiter):
 def trim(ctx, coll):
     string = ensure_string_singleton(coll)
     return string.strip()
+
+
+def encode(ctx, coll, format):
+    if not coll:
+        return []
+
+    str_to_encode = coll[0] if isinstance(coll, list) else coll
+    if not str_to_encode:
+        return []
+
+    if format in ["urlbase64", "base64url"]:
+        encoded = base64.b64encode(str_to_encode.encode()).decode()
+        return encoded.replace("+", "-").replace("/", "_")
+
+    if format == "base64":
+        return base64.b64encode(str_to_encode.encode()).decode()
+
+    if format == "hex":
+        return "".join([hex(ord(c))[2:].zfill(2) for c in str_to_encode])
+
+    return []
+
+
+def decode(ctx, coll, format):
+    if not coll:
+        return []
+
+    str_to_decode = coll[0] if isinstance(coll, list) else coll
+    if not str_to_decode:
+        return []
+
+    if format in ["urlbase64", "base64url"]:
+        decoded = str_to_decode.replace("-", "+").replace("_", "/")
+        return base64.b64decode(decoded).decode()
+
+    if format == "base64":
+        return base64.b64decode(str_to_decode).decode()
+
+    if format == "hex":
+        if len(str_to_decode) % 2 != 0:
+            raise ValueError("Decode 'hex' requires an even number of characters.")
+        return "".join(
+            [chr(int(str_to_decode[i : i + 2], 16)) for i in range(0, len(str_to_decode), 2)]
+        )
+
+    return []
 
 
 def join(ctx, coll, separator=""):
