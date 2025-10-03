cb2c7d56dc2aa90d1d50c1d7cb72a136ed3b0de4
Pavel Rozhkov <prozskov@gmail.com>
2025-10-03 10:26:18 +0200

    Parametrize helper tests for `compile_as_first` and `compile_as_array` to improve reusability and coverage in exception and result validation Ref: https://github.com/beda-software/fhirpath-py/issues/63

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,6 @@
 from fhirpathpy import compile_as_first, compile_as_array
 import fhirpy_types_r4b as r4b
+import pytest
 
 PATIENT_DATA = {
     "resourceType": "Patient",
@@ -10,40 +11,31 @@ PATIENT_RESOURCE = r4b.Patient(**PATIENT_DATA)
 
 EXPRESSION = "Patient.name.given"
 
-
-def compile_as_first_with_dict_test():
-    patient_given = compile_as_first(EXPRESSION, dict, str)
-    assert patient_given(PATIENT_DATA) == "First"
-
-
-def compile_as_first_with_resource_test():
-    patient_given = compile_as_first(EXPRESSION, r4b.Patient, str)
-    assert patient_given(PATIENT_RESOURCE) == "First"
-
-
-def compile_as_array_with_dict_test():
-    patient_given = compile_as_array(EXPRESSION, dict, list[str])
-    assert patient_given(PATIENT_DATA) == ["First", "Middle"]
+@pytest.mark.parametrize(
+    ("fn", "resource", "path", "input_type", "output_type", "expected"),
+    [
+        (compile_as_first, PATIENT_DATA, EXPRESSION, dict, str, "First"),
+        (compile_as_first, PATIENT_RESOURCE, EXPRESSION, r4b.Patient, str, "First"),
+        (compile_as_array, PATIENT_DATA, EXPRESSION, dict, list[str], ["First", "Middle"]),
+        (compile_as_array, PATIENT_RESOURCE, EXPRESSION, r4b.Patient, list[str], ["First", "Middle"]),
+    ],
+)
+def compile_as_test(fn, resource, path, input_type, output_type, expected):
+    wrapper_fn = fn(path, input_type, output_type)
+    assert wrapper_fn(resource) == expected
 
 
-def compile_as_array_with_resource_test():
-    patient_given = compile_as_array(EXPRESSION, r4b.Patient, list[str])
-    assert patient_given(PATIENT_RESOURCE) == ["First", "Middle"]
-
-
-def compile_as_first_exception_test():
-    patient_given = compile_as_first(EXPRESSION, r4b.Observation, str)
+@pytest.mark.parametrize(
+    ("fn", "resource", "path", "input_type", "output_type", "expected"),
+    [
+        (compile_as_first, PATIENT_RESOURCE, EXPRESSION, r4b.Observation, str, "Resource type is <class 'fhirpy_types_r4b.Patient'>, expected <class 'fhirpy_types_r4b.Observation'>"),
+        (compile_as_array, PATIENT_RESOURCE, EXPRESSION, r4b.Observation, list[str], "Resource type is <class 'fhirpy_types_r4b.Patient'>, expected <class 'fhirpy_types_r4b.Observation'>"),
+    ],
+)
+def exception_compile_as_test(fn, resource, path, input_type, output_type, expected):
+    wrapper_fn = fn(path, input_type, output_type)
     try:
-        patient_given(PATIENT_RESOURCE)
+        wrapper_fn(resource)
         assert False, "Expected exception not raised"
     except Exception as e:
-        assert str(e) == "Resource type is <class 'fhirpy_types_r4b.Patient'>, expected <class 'fhirpy_types_r4b.Observation'>"
-
-
-def compile_as_array_exception_test():
-    patient_given = compile_as_array(EXPRESSION, r4b.Observation, list[str])
-    try:
-        patient_given(PATIENT_RESOURCE)
-        assert False, "Expected exception not raised"
-    except Exception as e:
-        assert str(e) == "Resource type is <class 'fhirpy_types_r4b.Patient'>, expected <class 'fhirpy_types_r4b.Observation'>"
+        assert str(e) == expected
